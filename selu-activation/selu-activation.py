6c4567427f61f67ff15f8d24b1cfd9dc7a03d4fb
Akinsanya Joel <akinsanyajoel82@gmail.com>
2026-07-22 20:15:59 +0100

    Solve selu-activation

---
+++ selu-activation/selu-activation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def selu(x, lam=1.0507009873554804934193349852946, alpha=1.6732632423543772848170429916717):
+    """
+    Apply SELU activation element-wise.
+    Returns a list of floats rounded to 4 decimal places.
+    """
+    x_arr = np.asarray(x, dtype=float)
+    result = np.where(x_arr > 0, lam * x_arr, lam * alpha * (np.exp(x_arr) -1))
+
+    result = np.round(result, 4)
+
+    if result.ndim == 0:
+        return [float(result)]
+    else:
+        return result.tolist()
